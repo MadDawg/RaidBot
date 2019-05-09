a9b7a41a68b4764bdc3e80d86838b127eb642ac0
Cornell Washington <cdwash1991@gmail.com>
2019-05-09 17:59:04 -0500

    added profile command

---
+++ commands/profile.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'profile',
+    aliases: [],
+    description: 'View profile player\'s profile',
+    guildOnly: true,
+    args: true,
+    usage: '[name]',
+    admin: false,
+    leader: false,
+
+    execute(message, args, raids, players){}
+}
