647a48f030c7c93614299d1d697ab173d1ace9b9
Cornell Washington <cdwash1991@gmail.com>
2019-05-26 13:43:53 -0500

    allow whitespace in raid names, but remove date parameter

--- commands/create.js
+++ commands/create.js
@@ -1,51 +1,37 @@
+const Player = require('../player.js');
+const Raid = require('../raid.js');
+const PlayerFinder = require('../lib/findplayer.js');
+
 module.exports = {
     name: 'create',
-    aliases: ['createraid'],
-    description: 'Create raid',
+    aliases: ['createraid', 'makeraid', 'hostraid', 'host'],
+    description: 'Create a raid',
     guildOnly: true,
     args: true,
-    usage: '<name> <level> [time]',
+    usage: '<name>',
     admin: false,
     leader: false,
-    minArgs: 2,
+    //minArgs: 2,
 
     execute(message, args, raids, players){
-        name = args[0].toLowerCase();
-        level = args[1].toLowerCase();
+        const name = args.join(" ");
 
-        if (isNaN(level) && level != "high" && level != "low"){
-            return message.reply(`Invalid value entered for level.`);
+        const player = PlayerFinder.findPlayer(message.author.id, players);
+
+        if (player.bracket < 1){
+            return message.reply(`set/check your profile's level bracket before trying to start a raid.`);
         }
 
-        let index = raids.findIndex(x => x.name === name);
+        let index = raids.findIndex(x => x.name.toLowerCase() === name.toLowerCase());
         if (index > -1){
-            return message.reply(`Raid **${name}** already exists.`)
+            return message.reply(`raid **${raids[index].name}** already exists.`)
         }
 
-        const raid = new Raid(name, level, message.author.id, message.guild.id);
+        const raid = new Raid(name, player, message.guild.id);
         raids.push(raid);
-        let reply = `Raid **${raid.name}** created!`;
+        let reply = `raid **${raid.name}** created!`;
 
-
-        let player = undefined;
-        index = players.findIndex(x => x.uid === message.author.id);
-        if (index > -1){ player = players[index]; }
-        else{
-            player = new Player(message.author.id);
-            players.push(player);
-        }
         player.active = raid;
 
-        reply += `\nSwitched active raid to **${raid.name}**.`
-
-        if (args.length > 2){
-            date_raw_str = args.splice(0,2);
-            date_str = Date(date_raw_str.join(" "));
-            if (date_str == "Invalid Date"){
-                reply += `\nInvalid date/time was ignored.`;
-            }
-            else{ raid.setTime(Date(date_str)); }
-        }
-        return message.reply(reply);
-    }
+        reply += `\nNow managing **${raid.name}**.`
 }
